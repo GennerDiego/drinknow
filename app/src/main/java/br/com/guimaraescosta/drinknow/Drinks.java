c807635b450234cd77972ac0e4655d37764716f9
jaiguimaraes <jaiane.guimaraes@gmail.com>
2018-01-21 22:08:43 -0200

    Lista Objetos

---
+++ app/src/main/java/br/com/guimaraescosta/drinknow/Drinks.java
@@ -0,0 +1,44 @@
+package br.com.guimaraescosta.drinknow;
+
+/**
+ * Created by jguimara on 21/01/2018.
+ */
+
+public class Drinks {
+
+    private String name;
+    private String description;
+    private int quantity;
+
+
+    public Drinks(String nome, String descricao) {
+        this.name = nome;
+        this.description = descricao;
+        //this.quantity = quantidade;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
--- app/src/main/java/br/com/guimaraescosta/drinknow/DrinksActivity.java
+++ app/src/main/java/br/com/guimaraescosta/drinknow/DrinksActivity.java
@@ -1,20 +1,31 @@
 package br.com.guimaraescosta.drinknow;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
-public class DrinksActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class DrinksActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
 
-        String [] drinks = {"Alexander (licor de cacau, conhaque e creme)","Alice (coquetel sem álcool)","Batida cremosa (leite condensado, gelo, vodka)","Esmeralda (licor de menta e soda limonada)","Kir (licor de cassis e champanhe)","Lagoa Azul (blue curaçau e vodka)","Martini com cerejas","SHIRLEY TEMPLE", "VARIAÇÃO DA PIÑA COLADA SEM ÁLCOOL - VIRGIN COLADA", "VIRGIN ON THE BEACH"};
-        ListView drinks_list = findViewById(R.id.drinks_list);
-        ArrayAdapter<String> drinksArray = new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item,drinks);
-        drinks_list.setAdapter(drinksArray);
+        ListView lista = findViewById(R.id.drinks_list);
+        List<Drinks> drinks = allDrinks();
+        CustomGrid adapter = new CustomGrid(drinks, this);
+        lista.setAdapter(adapter);
+    }
+
+    private List<Drinks> allDrinks() {
+        return new ArrayList<>(Arrays.asList(
+                new Drinks("DAIKIRI", "Licor de cacau, conhaque e creme"),
+                new Drinks("Alice", "Blue curaçau e vodka"),
+                new Drinks("Lagoa Azul","blue curaçau e vodka" )));
+
+
     }
 }
