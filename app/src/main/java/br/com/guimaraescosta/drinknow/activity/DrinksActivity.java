fdfb65fa5605bb3c7c1c130a1226148ca50c0281
jaiguimaraes <jaiane.guimaraes@gmail.com>
2018-02-02 18:31:17 -0200

    List Layout 90%

--- app/src/main/java/br/com/guimaraescosta/drinknow/activity/DrinksActivity.java
+++ app/src/main/java/br/com/guimaraescosta/drinknow/activity/DrinksActivity.java
@@ -1,18 +1,35 @@
 package br.com.guimaraescosta.drinknow.activity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import br.com.guimaraescosta.drinknow.br.com.guimaraescosta.drinknow.adapter.AdapterTest;
 import br.com.guimaraescosta.drinknow.br.com.guimaraescosta.drinknow.adapter.GenericAdapter;
 import br.com.guimaraescosta.drinknow.br.com.guimaraescosta.drinknow.entity.Drink;
 import br.com.guimaraescosta.drinknow.R;
@@ -20,35 +37,79 @@ import br.com.guimaraescosta.drinknow.R;
 public class DrinksActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final ListView lista;
+        final ArrayAdapter<Drink> adapter;
+        final List<Drink> drinks;
+        final AdapterTest adapterT;
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
 
         //Fixed event image
         ImageView event_image = findViewById(R.id.event_image);
-        event_image.setImageResource(R.drawable.marriage);
+        event_image.setImageResource(R.drawable.marriage2);
 
        // Fixed event name
         TextView event_name = findViewById(R.id.event_name);
-        event_name.setText("Danilo e Letícia");
+        event_name.setText("(Camila)");
 
 
 
-        ListView lista = findViewById(R.id.drinks_list);
-        final List<Drink> drinks = allDrinks();
+        lista = findViewById(R.id.drinks_list);
+        lista.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        drinks = allDrinks();
+
+        LayoutInflater inflater = getLayoutInflater();
+        ViewGroup header = (ViewGroup)inflater.inflate(R.layout.header,lista,false);
+        header.bringToFront();
+        lista.addHeaderView(header);
 
 
 
-        lista.setAdapter(new GenericAdapter<Drink>(getApplicationContext(), drinks) {
+//        ArrayAdapter<Drink> adapterRE = new ArrayAdapter<Drink>(this,android.R.layout.simple_list_item_multiple_choice, drinks) ;
+//        lista.setAdapter(adapterRE);
+
+
+//        adapterT = new AdapterTest(this, android.R.layout.simple_list_item_multiple_choice, drinks);
+//        lista.setAdapter(adapterT);
+
+
+//        if(nome != null) {
+//            nome.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    if (nome.isChecked()) {
+//                        nome.setChecked(false);
+//                    } else {
+//                        nome.setChecked(true);
+//                        String teste = (String) nome.getText().toString();
+//
+//                    }
+//
+//                }
+//            });
+//        }
+
+        lista.setAdapter(new GenericAdapter<Drink>(getApplicationContext(),R.layout.row_layout,drinks) {
+
             @Override
             public View getMyView(int position, View convertView, ViewGroup parent, Drink drink) {
+
+
+
                 return viewDrink(parent, drink);
             }
         });
-    }
+
+
+
+
+}
 
     private List<Drink> allDrinks() {
         return new ArrayList<>(Arrays.asList(
-                new Drink("DAIKIRI", "Licor de cacau, conhaque e creme"),
+                new Drink("Daikiri", "Licor de cacau, conhaque e creme"),
                 new Drink("Alice", "Blue curaçau e vodka"),
                 new Drink("Lagoa Azul","blue curaçau e vodka"),
                 new Drink("Lagoa Azul","blue curaçau e vodka"),
@@ -58,24 +119,106 @@ public class DrinksActivity extends AppCompatActivity {
 
     private View viewDrink(ViewGroup parent, Drink drink){
 
-        View view = getLayoutInflater().inflate(R.layout.activity_drinks, parent, false);
-        TextView nome = view.findViewById(R.id.drink_name);
+        View view = getLayoutInflater().inflate(R.layout.row_layout, parent, false);
+       final CheckedTextView nome = view.findViewById(R.id.drink_name);
         TextView descricao = view.findViewById(R.id.drink_description);
-        ImageView imagem = view.findViewById(R.id.drink_image);
+      ImageView imagem = view.findViewById(R.id.drink_image);
+
+
+        final ListView lista = findViewById(R.id.drinks_list);
+        lista.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+            nome.setText(drink.getName());
+            descricao.setText(drink.getDescription());
+            imagem.setImageResource(R.drawable.sample_2);
+            imagem.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+            if(nome.getText().equals("Daikiri")) {
+               imagem.setImageResource(R.drawable.sample_0);
+            }
+
+            if(nome.getText().equals("Lagoa Azul")) {
+                imagem.setImageResource(R.drawable.sample_1);
+            }
 
 
 
-        nome.setText(drink.getName());
-        descricao.setText(drink.getDescription());
 
-        imagem.setImageResource(R.drawable.sample_1);
-        imagem.setScaleType(ImageView.ScaleType.CENTER_CROP);
 
-        if(nome.getText().equals("DAIKIRI")) {
-            imagem.setImageResource(R.drawable.sample_0);
-        }
+            nome.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(nome.isChecked()) {
+                        nome.setChecked(false);
+
+                    } else{
+                        nome.setChecked(true);
+                        String teste = nome.getText().toString();
+
+                    }
+
+                }
+            });
+//
+//        OnItemClickListener itemClickListener = new OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Object drink = lista.getItemAtPosition(i);
+//                String drinkString = drink.toString();
+//                if(nome.isChecked()) {
+//                    nome.setChecked(false);
+//                    lista.setItemChecked(i,false);
+//
+//                } else{
+//                    nome.setChecked(true);
+//                    String teste = nome.getText().toString();
+//                    lista.setItemChecked(i,true);
+//
+//                }
+//            }
+//        };
+//
+//        lista.setOnItemClickListener(itemClickListener);
+
+
+
+//        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//            @RequiresApi(api = Build.VERSION_CODES.M)
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Object drink = lista.getItemAtPosition(i);
+//
+//                String drinkString = drink.toString();
+//                String name = nome.getText().toString();
+//                if(nome.isChecked()) {
+//                    nome.setChecked(false);
+//
+//
+//                } else{
+//                    nome.setChecked(true);
+//                    String teste = nome.getText().toString();
+//                    lista.setItemChecked(i,true);
+//
+//
+//                }
+//
+//            }
+//        });
+//
+////
+
+
 
         return view;
     }
 
+
+
 }
+
+
+
+
+
--- app/src/main/java/br/com/guimaraescosta/drinknow/br/com/guimaraescosta/drinknow/adapter/GenericAdapter.java
+++ app/src/main/java/br/com/guimaraescosta/drinknow/br/com/guimaraescosta/drinknow/adapter/GenericAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 
 import java.util.List;
@@ -13,13 +14,14 @@ import java.util.List;
  * Created by genner.almeida on 21/01/18.
  */
 
-public abstract class GenericAdapter<T> extends BaseAdapter {
+public abstract class GenericAdapter<T> extends ArrayAdapter {
 
     LayoutInflater layoutInflater;
     Context context;
     private List<T> tList;
 
-    public GenericAdapter(Context context, List<T> tList) {
+    public GenericAdapter(Context context, int resource, List<T> tList) {
+        super(context, resource, tList);
         this.context = context;
         this.tList = tList;
         layoutInflater = LayoutInflater.from(context);
